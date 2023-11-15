51c2d0cb378d5e19fced3af574f5586ce8fb16f0
Bogfoot <bogfoot94@yahoo.com>
2023-11-15 11:54:27 +0100

    Using some of Rainers Code

---
+++ modelsvideo.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python3
+
+# switched to python3 on October 20th, 2020
+
+# based on the example given here: https://www.pythonforthelab.com/blog/step-by-step-guide-to-building-a-gui/
+# date: 19-22.2.2019
+# author: Rainer Kaltenbaek
+
+# # # # modified the script
+
+# in order to switch between Widows 10 and Linux just comment in/out the appoprite first few lines
+# in the initialize() function. the difference is in how OS finds the camera
+
+# right now, this definetly works for ONE camera on Windows 10. I have not tested it for more than one, maybe
+# that requires a few extra lines. Linux should work without a problem
+
+import cv2
+import numpy as np
+import sys
+
+
+class Camera:
+    def __init__(self, cam_num):
+        self.cap = None
+        self.cam_num = cam_num
+        self.mycount = 0
+
+    def get_frame(self):
+        ret, self.last_frame = self.cap.read()
+        self.mycount = self.mycount + 1
+        if not ret:
+            print("error in getting frame")
+        else:
+            print("got frame nr. %d" % (self.mycount))
+        return self.last_frame
+
+    def acquire_movie(self, num_frames):
+        movie = []
+        for _ in range(num_frames):
+            movie.append(self.get_frame())
+        return movie
+
+    def set_brightness(self, value):
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, value)
+
+    def get_brightness(self):
+        return self.cap.get(cv2.CAP_PROP_BRIGHTNESS)
+
+    def get_saturation(self):
+        return self.cap.get(cv2.CAP_PROP_SATURATION)
+
+    def __str__(self):
+        return "OpenCV Camera {}".format(self.cam_num)
+
+    def close_camera(self):
+        self.cap.release()
+
+    def initialize(self):
+        # Windows ##################
+        # self.cap = cv2.VideoCapture(self.cam_num)
+        ############################
+
+        # Linux ####################
+        videostr = "/dev/video%d" % (self.cam_num)  # Linux
+        self.cap = cv2.VideoCapture(videostr)  # Linux
+        print("video string: ", videostr)  # Linux
+        ############################
+
+        ##self.cap = cv2.VideoCapture("/dev/video4")
+        # cv2.CAP_DSHOW
+        if not self.cap:
+            print("error opening webcam\n")
+            sys.exit(1)
+        # self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, -4)
+        ##print("HELLO WORLD")
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+        ##print("test 1")
+        # seemingly not supported: self.cap.set(cv2.CAP_PROP_EXPOSURE, 0.1)
+        ##print("test 2")
+        self.cap.set(cv2.CAP_PROP_SATURATION, 1.0)
+        ##print("test 3")
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
+        ##self.cap.set(cv2.CAP_PROP_CONTRAST, 0.1)
+        ##self.cap.set(cv2.CAP_PROP_GAIN, 0.0)
+        # self.cap.set(cv2.CAP_PROP_BACKLIGHT, 1)
+        ##self.cap.set(cv2.CAP_PROP_GAMMA, 120)
+        # self.CAP_PROP_AUTO_EXPOSURE = -4
+        # self.CAP_PROP_AUTO_WB = 0.25
+        # self.CAP_PROP_AUTOFOCUS = 0.25
+
+    def myCM(self, mydata):
+        """
+        calculate the center of mass of mydata
+
+        mydata is assumed to be a 2D numpy array of some arbitrary size (M, N)
+        the values of the array elements are considered to be the mass density
+        the indices in the array are the x-y coordinates.
+
+        returns an ndarray with two elements for the CM coordinates
+        """
+        x = np.arange(mydata.shape[0])
+        y = np.arange(mydata.shape[1])
+        # make 2D arrays out of these ranges:
+        xarr = np.zeros(mydata.shape)
+        yarr = np.zeros(mydata.shape)
+        for i in x:
+            xarr[i, :] = x[i]
+        for i in y:
+            yarr[:, i] = y[i]
+        xdata = xarr * mydata
+        ydata = yarr * mydata
+        xsum = np.sum(xdata)
+        ysum = np.sum(ydata)
+        msum = np.sum(mydata)
+        myCM = np.array([xsum, ysum])
+        # print(xsum/msum)
+        myCM /= msum
+        return myCM
+
+    def myQuad(self, cpos, mydata):
+        """
+        calculates the for quadrant sums and returns the X_diff and Y_diff
+        """
+        totalSum = 1.0 * np.sum(mydata)
+        leftData = 1.0 * mydata[:, : cpos[0]]
+        rightData = 1.0 * mydata[:, cpos[0] :]
+        topData = 1.0 * mydata[: cpos[1], :]
+        bottomData = mydata[cpos[1] :, :]
+        diffX = int(round((np.sum(rightData) - np.sum(leftData)) / totalSum))
+        diffY = int(round((np.sum(topData) - np.sum(bottomData)) / totalSum))
+        # print("diff Y: ", diffX)
+        # print("diff X: ", diffY)
+        return np.array((diffX, diffY))
+
+    def getCM(self):
+        gray = cv2.cvtColor(self.last_frame, cv2.COLOR_BGR2GRAY)
+        # print(gray.shape)
+        pos = self.myCM(gray)
+        return pos
+
+    def getQuad(self, cpos):
+        gray = cv2.cvtColor(self.last_frame, cv2.COLOR_BGR2GRAY)
+        # print(gray.shape)
+        pos = self.myQuad(cpos, gray)
+        return pos
+
+    def getSum(self):
+        gray = cv2.cvtColor(self.last_frame, cv2.COLOR_BGR2GRAY)
+        # print("gain: ", self.cap.get(cv2.CAP_PROP_GAIN))
+        # print("saturation: ", self.cap.get(cv2.CAP_PROP_SATURATION))
+        # print("exposure: ", self.cap.get(cv2.CAP_PROP_EXPOSURE))
+        return np.sum(gray)
+
+
+# %%
+if __name__ == "__main__":
+    cam = Camera(0)
+    cam.initialize()
+    print(cam)
+    frame = cam.get_frame()
+    print(frame)
+    print(frame.shape)
+    cam.close_camera()
+
+    # -------------- added by Kristof on 29.3.2021
+    import matplotlib.pyplot as plt
+    import time
+
+    boxsize = 30
+    averagingsize = 8
+    fontsize = 20
+
+    xmax = np.where(frame[:, :, 2] == frame[:, :, 2].max())[0][0]
+    ymax = np.where(frame[:, :, 2] == frame[:, :, 2].max())[1][0]
+
+    closeup = frame[
+        xmax - boxsize // 2 : xmax + boxsize // 2,
+        ymax - boxsize // 2 : ymax + boxsize // 2,
+        2,
+    ]
+
+    xstd = (
+        frame[:, ymax - averagingsize // 2 : ymax + averagingsize // 2, 2]
+        .mean(axis=1)
+        .std()
+    )
+    ystd = (
+        frame[xmax - averagingsize // 2 : xmax + averagingsize // 2, :, 2]
+        .mean(axis=0)
+        .std()
+    )
+
+    fig, ax = plt.subplots(3)
+    ax[0].imshow(frame[:, :, 2])
+    ax[1].imshow(closeup)
+    # ax[2].imshow(np.ones((100,100)))
+    ax[2].text(x=0.50, y=0.8, s=r"$\sigma_x = {:.2f}$".format(xstd), fontsize=fontsize)
+    ax[2].text(x=0.50, y=0.20, s=r"$\sigma_y = {:.2f}$".format(ystd), fontsize=fontsize)
+
+    plt.show()
---
+++ viewsvideo.py
@@ -0,0 +1,332 @@
+#!/usr/bin/python3
+
+# switched to python3 on October 20th, 2020
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLineEdit, QSpinBox, QLabel
+from pyqtgraph import ImageView
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from PyQt5.QtCore import QTimer, QLocale
+
+import numpy as np
+import time
+
+from modelsvideo import Camera
+
+# import QGraphics
+
+class StartWindow(QMainWindow):
+  def __init__(self, camera = None, save_frames=False):
+    super(StartWindow, self).__init__()
+    self.camera = camera
+    #--- added by Kristof
+    self.save_frames = save_frames
+    if self.save_frames:
+        self.time_array = []
+        self.frames_array = []
+    #---
+    
+    #self.camera.initialize()
+    self.central_widget = QWidget()
+    self.central_widget.setObjectName("central_widget") 
+    self.central_widget.setLocale(QLocale("US_en"));
+    self.set_frame_geometry(640, 480)
+    super(StartWindow, self).resize(self.areaWidth + 600, self.areaHeight)
+
+    self.XstepSize = 1
+    self.YstepSize = 1
+    self.CMx = 0
+    self.CMy = 0
+    self.centerCircle = True
+    self.circleRadius = 10;
+
+    self.videoArea = QtCore.QRectF(600, 0, self.areaWidth, self.areaHeight)
+
+    self.scene = QtGui.QGraphicsScene(self.videoArea)
+
+    pen = QtGui.QPen()
+    pen.setColor(QtGui.QColor(0,200,0,255))
+    #pen.setcolor(Qt::red)
+    brush = QtGui.QBrush(QtGui.QColor(0,200,0,255))
+    #QBrush brush(Qt::red, Qt::CrossPattern)
+    self.centerCircle = self.scene.addEllipse(QtCore.QRectF(self.CMx-self.circleRadius,self.CMy-self.circleRadius,2*self.circleRadius,2*self.circleRadius), pen, brush);
+
+    self.graphicsView = QtWidgets.QGraphicsView(self.central_widget)
+    self.graphicsView.setGeometry(QtCore.QRect(10, 40, 601, 411))
+    self.graphicsView.setObjectName("graphicsView")
+    self.graphicsView.setScene(self.scene)
+
+    self.buttonUp = QPushButton('Up', self.central_widget)
+    self.buttonUp.setObjectName("buttonUp")  
+    self.buttonDown = QPushButton('Down', self.central_widget) 
+    self.buttonDown.setObjectName("buttonDown") 
+    self.buttonLeft = QPushButton('Left', self.central_widget)
+    self.buttonLeft.setObjectName("buttonLeft") 
+    self.buttonRight = QPushButton('Right', self.central_widget) 
+    self.buttonRight.setObjectName("buttonRight") 
+    self.buttonQUIT = QPushButton('QUIT', self.central_widget)
+    self.buttonQUIT.setObjectName("buttonQUIT") 
+    self.DXspinBox = QSpinBox(self.central_widget)
+    self.DXspinBox.setObjectName("DXspinBox") 
+    self.DYspinBox = QSpinBox(self.central_widget)
+    self.DYspinBox.setObjectName("DYspinBox") 
+    self.XspinBox = QSpinBox(self.central_widget)
+    self.XspinBox.setObjectName("XspinBox") 
+    self.YspinBox = QSpinBox(self.central_widget)
+    self.YspinBox.setObjectName("YspinBox")
+    font = QtGui.QFont()
+    self.CMyPos = QLineEdit(self.central_widget)
+    self.CMyPos.setObjectName("CMyPos")
+    self.CMxPos = QLineEdit(self.central_widget)
+    self.CMxPos.setObjectName("CMxPos")
+    Palette= QtGui.QPalette()
+    Palette.setColor(QtGui.QPalette.Text, QtGui.QColor(180,180,180,255))
+    self.CMxPos.setPalette(Palette)
+    self.CMyPos.setPalette(Palette)
+    font.setPointSize(30)
+    font.setBold(True)
+    font.setWeight(75) 
+    self.QyPos = QLineEdit(self.central_widget)
+    self.QyPos.setObjectName("CMyPos")
+    self.QxPos = QLineEdit(self.central_widget)
+    self.QxPos.setObjectName("CMxPos")
+    self.QxPos.setReadOnly(True)
+    self.QyPos.setReadOnly(True)
+    self.QxPos.setFont(font)
+    self.QyPos.setFont(font)
+    self.QxPos.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+    self.QyPos.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+
+    self.sumIntensity = QLineEdit(self.central_widget)
+    self.sumIntensity.setObjectName("sumIntensity")
+    self.sumIntensity.setReadOnly(True)
+    self.sumIntensity.setFont(font)
+    self.sumIntensity.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+
+    self.buttonUp.setGeometry(QtCore.QRect(80,40,90,25))
+    self.buttonDown.setGeometry(QtCore.QRect(80,100,90,25))
+    self.buttonLeft.setGeometry(QtCore.QRect(30, 70, 90, 25))
+    self.buttonRight.setGeometry(QtCore.QRect(130, 70, 90, 25))
+    self.buttonQUIT.setGeometry(70,150,100,60) 
+
+    self.DXspinBox.setGeometry(QtCore.QRect(320, 50, 80, 25))
+    self.DYspinBox.setGeometry(QtCore.QRect(320, 90, 80, 25))
+    self.XspinBox.setGeometry(QtCore.QRect(490, 50, 80, 25))
+    self.YspinBox.setGeometry(QtCore.QRect(490, 90, 80, 25))
+
+    self.CMyPos.setGeometry(QtCore.QRect(390, 185,180, 45))
+    self.CMxPos.setGeometry(QtCore.QRect(390, 135, 180, 45))
+
+    self.QyPos.setGeometry(QtCore.QRect(390, 290,180, 45))
+    self.QxPos.setGeometry(QtCore.QRect(390, 240, 180, 45))
+
+    self.sumIntensity.setGeometry(QtCore.QRect(350, 360, 220, 45))
+
+
+    self.DXspinBox.setValue(self.XstepSize)
+    self.DXspinBox.setMinimum(1)
+    self.DXspinBox.setMaximum(20)
+    self.DYspinBox.setValue(self.XstepSize)
+    self.DYspinBox.setMinimum(1)
+    self.DYspinBox.setMaximum(20)
+
+
+    self.XspinBox.setMinimum(0)
+    self.XspinBox.setMaximum(self.areaWidth)
+    self.XspinBox.setValue(self.Xcenter)
+
+    self.YspinBox.setMinimum(0)
+    self.YspinBox.setMaximum(self.areaHeight)
+    self.YspinBox.setValue(self.Ycenter)
+
+    self.Ylabel = QLabel(self.central_widget)
+    self.Ylabel.setGeometry(QtCore.QRect(450, 90, 60, 20))
+    self.Ylabel.setObjectName("Ylabel")
+
+    font.setPointSize(24)
+    self.buttonQUIT.setFont(font)
+
+    self.YposLabel = QLabel(self.central_widget)
+    self.YposLabel.setGeometry(QtCore.QRect(200, 180, 180, 50))
+    self.YposLabel.setFont(font)
+    self.YposLabel.setObjectName("YposLabel")
+
+    self.XposLabel = QLabel(self.central_widget)
+    self.XposLabel.setGeometry(QtCore.QRect(200, 130, 180,50))
+    self.XposLabel.setFont(font)
+    self.XposLabel.setObjectName("XposLabel")
+
+    self.sumLabel = QLabel(self.central_widget)
+    self.sumLabel.setGeometry(QtCore.QRect(115, 355, 250,50))
+    self.sumLabel.setFont(font)
+    self.sumLabel.setObjectName("sumLabel")
+
+    self.QXlabel = QtWidgets.QLabel(self.central_widget)
+    self.QXlabel.setFont(font)
+    self.QXlabel.setGeometry(QtCore.QRect(250, 235, 130, 50))
+    self.QXlabel.setObjectName("QXlabel")
+    self.QYlabel = QtWidgets.QLabel(self.central_widget)
+    self.QYlabel.setFont(font)
+    self.QYlabel.setGeometry(QtCore.QRect(250, 285, 130, 50))
+    self.QYlabel.setObjectName("QYlabel")
+
+
+    self.Xlabel = QLabel(self.central_widget)
+    self.Xlabel.setGeometry(QtCore.QRect(450, 50, 60, 20))
+    self.Xlabel.setObjectName("Xlabel")
+
+
+    font.setPointSize(30)
+    self.CMxPos.setFont(font)
+    self.CMxPos.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+    self.CMyPos.setFont(font)
+    self.CMyPos.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+    self.CMxPos.setReadOnly(True)
+    self.CMxPos.setObjectName("CMxPos")    
+    self.CMyPos.setReadOnly(True)
+    self.CMyPos.setObjectName("CMyPos")
+
+    font.setPointSize(20)
+    self.DXlabel = QtWidgets.QLabel(self.central_widget)
+    self.DXlabel.setGeometry(QtCore.QRect(240, 50, 71, 20))
+    self.DXlabel.setObjectName("DXlabel")
+    self.DYlabel = QtWidgets.QLabel(self.central_widget)
+    self.DYlabel.setGeometry(QtCore.QRect(240, 90, 71, 20))
+    self.DYlabel.setObjectName("DYlabel")    
+
+
+    self.buttonUp.clicked.connect(self.stepUp)
+    self.buttonDown.clicked.connect(self.stepDown)
+    self.buttonLeft.clicked.connect(self.stepLeft)
+    self.buttonRight.clicked.connect(self.stepRight)
+    self.buttonQUIT.clicked.connect(self.myquit)
+
+    self.image_view = ImageView(self.central_widget)
+    self.image_view.ui.histogram.hide()
+    self.image_view.ui.roiBtn.hide()
+    self.image_view.ui.menuBtn.hide()
+    #self.image_view.roi.hide()
+    #self.image_view.roiCurve.hide()
+    #self.image_view.normRgn.hide()
+    #self.image_view.normRoi.hide()
+    #self.image_view.ui.roiPlot.hideAxis('bottom')
+    #self.image_view.timeLine.hide()
+    #self.image_view.ui.splitter.hide()
+    #self.image_view.autoRange = False
+    self.image_view.setGeometry(self.videoArea.toRect())
+    self.image_view.show()
+
+    self.timer = QTimer(self)
+    self.timer.timeout.connect(self.update_image)
+    self.timer.setInterval(100)
+    self.timer.start()
+    #self.drawCircle()
+        
+    self.retranslateUi(self.central_widget)
+    QtCore.QMetaObject.connectSlotsByName(self.central_widget)
+
+    self.setCentralWidget(self.central_widget)
+
+  def retranslateUi(self, mywidget):
+    _translate = QtCore.QCoreApplication.translate
+    mywidget.setWindowTitle(_translate("central_widget", "PSeye Capture"))
+    self.buttonLeft.setText(_translate("central_widget", "Left"))
+    self.buttonRight.setText(_translate("central_widget", "Right"))
+    self.buttonUp.setText(_translate("central_widget", "Up"))
+    self.buttonDown.setText(_translate("central_widget", "Down"))
+    self.buttonQUIT.setText(_translate("central_widget", "QUIT"))
+    self.DXlabel.setText(_translate("central_widget", "X stepsize:"))
+    self.DYlabel.setText(_translate("central_widget", "Y stepsize:"))
+    self.Xlabel.setText(_translate("central_widget", "C_x:"))
+    self.Ylabel.setText(_translate("central_widget", "C_y:"))   
+    self.CMxPos.setText(_translate("central_widget", "131"))
+    self.CMyPos.setText(_translate("central_widget", "131"))
+    self.XposLabel.setText(_translate("central_widget", "CM_x - C_x:"))
+    self.YposLabel.setText(_translate("central_widget", "CM_y - C_y:"))
+    Palette = self.XposLabel.palette()
+    Palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(180,180,180,255))
+    self.XposLabel.setPalette(Palette)
+    self.YposLabel.setPalette(Palette)
+    self.sumLabel.setText(_translate("central_widget", "Sum intensity:"))
+    self.QXlabel.setText(_translate("central_widget", "Quad_x:"))
+    self.QYlabel.setText(_translate("central_widget", "Quad_y:"))
+
+  def set_frame_geometry(self, w, h):
+    self.areaWidth = w
+    self.areaHeight = h
+    self.Xcenter = int(round(w))/2
+    self.Ycenter = int(round(h))/2
+
+  def drawCircle(self):
+    #self.graphicsView.paint() 
+    print("boohoo")
+
+  def update_image(self):
+    frame = self.camera.get_frame()
+    
+    #--- added by Kristof
+    if self.save_frames:
+        self.time_array.append(time.time())
+        self.frames_array.append(frame)
+    #---
+
+    self.image_view.setImage(frame.T)
+    (myCMy, myCMx) = self.camera.getCM()
+    self.CMx = myCMx-self.Xcenter
+    self.CMy = myCMy-self.Ycenter
+    Xstr = "{:.2f}".format(self.CMx)
+    Ystr = "{:.2f}".format(self.CMy)
+    self.CMxPos.setText(Xstr)
+    self.CMyPos.setText(Ystr)
+    (QuadX, QuadY) = self.camera.getQuad(np.array((round(self.Xcenter), round(self.Ycenter))))
+    Xstr = "{:.2f}".format(100*QuadX)
+    Ystr = "{:.2f}".format(100*QuadY)
+    self.QxPos.setText(Xstr)
+    self.QyPos.setText(Ystr)
+    Isum = self.camera.getSum()
+    IsumText = "{0:d}".format(Isum)
+    self.sumIntensity.setText(IsumText)
+
+  def stepUp(self):
+    self.Ycenter += self.DYspinBox.value()
+    #print("up");
+    self.YspinBox.setValue(self.Ycenter)
+
+  def stepDown(self):
+    self.Ycenter -= self.DYspinBox.value()
+    self.YspinBox.setValue(self.Ycenter)
+
+  def stepLeft(self):
+    self.Xcenter -= self.DXspinBox.value()
+    self.XspinBox.setValue(self.Xcenter)
+
+  def stepRight(self):
+    self.Xcenter += self.DXspinBox.value()
+    self.XspinBox.setValue(self.Xcenter)
+
+  def myquit(self):
+    self.camera.close_camera()
+    self.image_view.close()
+    #if timer.isActive():
+    self.timer.stop()
+    
+    #---added by Kristof
+    if self.save_frames:
+        np.savez(file="recorded_frames_{:s}".format(time.strftime("%Y-%m-%d_%H-%M-%S")), time=self.time_array, frames=self.frames_array)
+    #---
+    return self.close()
+
+if __name__ == '__main__':
+  print("IN THE MAIN OF >>webcam.py<<")
+  cam = Camera(0)
+  cam.initialize()
+  print(cam)
+  frame = cam.get_frame() #to je problem
+  print(frame)
+  cam.set_brightness(0.5)
+  print(cam.get_brightness())
+  cam.set_brightness(1)
+  print(cam.get_brightness())
+  cam.close_camera()
