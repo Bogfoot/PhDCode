4efd67f6118dc78f58b3b11e903856b787afcaf9
bogfoot <bogfoot94@yahoo.com>
2023-12-24 17:54:28 +0100

    upd

--- Sagnac_Interference_Measurements/RayTracer/laser.py
+++ Sagnac_Interference_Measurements/RayTracer/laser.py
@@ -12,23 +12,22 @@ and therefore will not be clipped by lenses.
 from raytracing import *
 
 
-def exampleCode(comments=None):
+def SagnacWaistEvolution(comments=None):
     # Demo #18: Laser beam and vendor lenses
     path = LaserPath()
     path.label = TITLE
     path.append(Space(d=50))
-    path.append(thorlabs.AC254_050_A())
+    path.append(Lens(f=250, diameter=25.4, label="Pump Lens"))
+    path.append(Space(d=240))
+    path.append(DielectricSlab(n=2.14, thickness=50, diameter=1, label="LiNbO3 PPLN"))
+    path.append(Space(d=175))
+    path.append(Lens(f=200, diameter=25.4, label="SPDC Lens"))
     path.append(Space(d=50))
-    path.append(thorlabs.AC254_050_A())
-    path.append(Space(d=150))
-    path.append(eo.PN_33_921())
-    path.append(Space(d=50))
-    path.append(eo.PN_88_593())
-    path.append(Space(d=180))
-    path.append(olympus.LUMPlanFL40X())
-    path.append(Space(d=10))
-    path.display(beams=[GaussianBeam(w=1.0100, z=1)], comments=comments)
+    # (self, q: complex = None, w: float = None, R: float = inf, n: float = 1.0, wavelength=0.0006328, z=0)
+    path.display(
+        beams=[GaussianBeam(wavelength=780.24e-9, w=3.6, z=1)], comments=comments
+    )
 
 
 if __name__ == "__main__":
-    exampleCode()
+    SagnacWaistEvolution()
