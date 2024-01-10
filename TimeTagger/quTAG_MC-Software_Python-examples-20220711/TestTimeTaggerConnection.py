cfd2b589610eaf906c1eaf1283e2495ff79845ce
bogfoot <bogfoot94@yahoo.com>
2024-01-10 11:38:25 +0100

    update

---
+++ TimeTagger/quTAG_MC-Software_Python-examples-20220711/TestTimeTaggerConnection.py
@@ -0,0 +1,42 @@
+import time
+
+try:
+    import QuTAG_MC as qt
+except:
+    print(f"Didn't find QuTAG or something went wrong.")
+
+#%%  test connecting to time tagger
+channel_1 = 1
+channel_2 = 2
+
+
+# Initialize the quTAG device
+tt = qt.QuTAG()
+
+# Get the timebase (the resolution) from the quTAG. It is used as time unit by many other functions.
+timebase = tt.getTimebase()
+print("Device timebase:", timebase, "s")
+
+
+# Set the exposure or integration time in milliseconds, range = 0..65535
+exposure_time_seconds = 0.5
+tt.setExposureTime(int( 1000 * exposure_time_seconds ))  # ms Counting
+
+tt.enableChannels((1, 2))  # Enables channel 0,2,3
+
+time.sleep(1)
+
+data, updates = tt.getCoincCounters()
+
+# wait a little to get the device started calibrating
+time.sleep(1)
+
+data, updates = tt.getCoincCounters()
+print("Updates since last call: ", updates, "| Data: ", data)
+print("channel: ", channel_1, ", counts: ", data[channel_1])
+print("channel: ", channel_2, ", counts: ", data[channel_2])
+print("channel: coincidences, counts: ", data[5])
+
+print(tt.discover())
+
+tt.deInitialize()
