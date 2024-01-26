86b01e36edc83513dcae1b73dc9bee92691cbdae
bogfoot <bogfoot94@yahoo.com>
2024-01-26 21:29:57 +0100

    upd

--- TimeTagger/quTAG_MC-Software_Python-examples-20220711/TestOven.py
+++ TimeTagger/quTAG_MC-Software_Python-examples-20220711/TestOven.py
@@ -1,33 +1,18 @@
 # %%  test connect to oven
 
+import time
+
 import OC
 
-# import OvenLibV2
-
-# usb_port = "/dev/ttyUSB0"
-# oven = serial.Serial(port=usb_port)
-#
-#
-# oven = OO.OvenController(oven, port=usb_port)
-# oven.enable_oven()
-#
-# print("current T = ", oven.check_temperature(), " C")
-# oven.close()
-#
-# usb_port = "COM7"
-usb_port = "/dev/ttyUSB0"  # This is for use on linux, you can also use /dev/bus/usb/... maybe depending on your setup
+usb_port = "/dev/ttyUSB0"
 
 
 print("OC.OC")
 oven = OC.OC(usb_port)  # OC3 Code from them
-# oven = OvenLibV2.OvenController(usb_port)  # OC3 Code from them
-
 print("Enable")
-# oven.enable_oven()
 oven.enable()
 print("Setting temp")
-oven.set_temperature(40)
-# print("Sleep for 10 seconds to see a different temperature")
-# time.sleep(10)
+oven.set_temperature(45)
+time.sleep(10)
 print(oven.temperature[0])
-# print(oven.check_temperature())
+oven.disable()
--- TimeTagger/quTAG_MC-Software_Python-examples-20220711/TestTimeTaggerConnection.py
+++ TimeTagger/quTAG_MC-Software_Python-examples-20220711/TestTimeTaggerConnection.py
@@ -1,5 +1,7 @@
 import time
 
+import OC
+
 try:
     import QuTAG_MC as qt
 except:
@@ -13,14 +15,17 @@ coincidances12 = 33
 
 # Initialize the quTAG device
 tt = qt.QuTAG()
-
+oven = OC.OC("/dev/ttyUSB0")
+oven.enable()
+oven.set_temperature(41.10)
+print(oven.get_temperature())
 # Get the timebase (the resolution) from the quTAG. It is used as time unit by many other functions.
 timebase = tt.getTimebase()
 print("Device timebase:", timebase, "s")
 
 
 # Set the exposure or integration time in milliseconds, range = 0..65535
-exposure_time_seconds = 0.5
+exposure_time_seconds = 1
 tt.setExposureTime(int(1000 * exposure_time_seconds))  # ms Counting
 
 time.sleep(1)
@@ -40,3 +45,5 @@ print("channel: coincidences, counts: ", data[coincidances12])
 print(tt.discover())
 
 tt.deInitialize()
+tt.getDeviceParams()
+oven.OC_close()
