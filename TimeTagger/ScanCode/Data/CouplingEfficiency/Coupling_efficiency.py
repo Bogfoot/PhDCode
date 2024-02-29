05a0a0448c30aa8981adb0c4ca1268a23615b2b4
bogfoot <bogfoot94@yahoo.com>
2024-02-29 17:10:59 +0100

    upd

---
+++ TimeTagger/ScanCode/Data/CouplingEfficiency/Coupling_efficiency.py
@@ -0,0 +1,11 @@
+data = [
+    [1724666, 1843451, 11663],
+    [482531, 611641, 1015],
+    [710880, 2403986, 5001],
+    [1691005, 2495838, 7480],
+    [114465, 153563, 18],
+    [25185, 30133, 130],
+]
+for item in data:
+    print("Coupling efficiency of 1st coupler", item[2] / item[0] * 100)
+    print("Coupling efficiency of 1st coupler", item[2] / item[1] * 100)
