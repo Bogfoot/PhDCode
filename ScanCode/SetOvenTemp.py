9600c78e5efb3275b8501efe9624f8f89c1ac54b
bogfoot <bogfoot94@yahoo.com>
2024-04-22 13:55:31 +0200

    upd

--- ScanCode/SetOvenTemp.py
+++ ScanCode/SetOvenTemp.py
@@ -5,11 +5,9 @@ from OC import OC
 
 oven = OC("/dev/ttyUSB0")
 temp = float(sys.argv[1])
-# ramp = float(sys.argv[2])
 
 oven.enable()
 sleep(1)
 
-# oven.set_ramp_rate(ramp)
 oven.set_temperature(temp)
 oven.OC_close()
